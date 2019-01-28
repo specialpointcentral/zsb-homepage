b2c46e940442db4fbd4ba47e8678a1537dbc06a2
spcsky <spcreply@126.com>
2019-01-28 21:53:40 +0800

    Revert "fix some problems"
    This reverts commit b0136ddf0a76fc811d0dadaf11cb4dac06c0367e.

--- ajax/postInfoAjax.php
+++ ajax/postInfoAjax.php
@@ -9,11 +9,11 @@
 header("Cache-Control: no-cache");
 function Curl($url)
 {
+   	$cookie = tempnam ("/tmp", "CURLCOOKIE");
     $ch2 = curl_init();
     curl_setopt($ch2, CURLOPT_URL, $url);
     curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch2, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($ch2, CURLOPT_REFERER, 'https://www.kuaidi100.com/');//模拟来路
   	curl_setopt($ch2, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36" );
     $temp = curl_exec($ch2);
     $httpCode = curl_getinfo($ch2,CURLINFO_HTTP_CODE); 
@@ -28,9 +28,7 @@ foreach ($_REQUEST as $key=>$value){
 }
 //print $urltemp;
 $p = Curl("https://www.kuaidi100.com/query".$urltemp);
-
 http_response_code($p["code"]);
-
 if(isset($_REQUEST["callback"])) 
     echo $_REQUEST["callback"]."(".$p["return"].")";
 else 
